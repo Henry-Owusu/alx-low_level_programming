9b54354d2b5a6c4bc88d3a6f21c324658d4ca6e9
Henry-Owusu <henryowusu023@gmail.com>
2023-03-16 16:59:52 -0700

    task complete

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: always 0 (success)
+ * main - A program tha prints a line using the printf function
+ * Return: 0 (success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
+}
