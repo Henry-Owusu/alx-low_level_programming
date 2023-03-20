6d8236ce6776e9c9313fd46e29ff1d10766d356f
Henry-Owusu <henryowusu023@gmail.com>
2023-03-20 02:12:01 -0700

    committed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: the program prints with a new line
- * Return - Always 0 (Success)
+ * main - A program that prints a line using the puts function
+ * Return:  0 (success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
--- 0x00-hello_world/5-print.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("with prooper grammar, but the outcome is a piece of art,\n"): /* prins the given text */
-	return 0; /* returns 0 */
-}
