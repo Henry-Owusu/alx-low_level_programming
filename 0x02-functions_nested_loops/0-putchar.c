dc70a22fa44cf58836cf145030d19f4c08bc679e
Henry-Owusu <henryowusu023@gmail.com>
2023-03-21 08:05:01 -0700

    committed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Write a program that prints _putchar, followed by a new line.
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+}
